110b3d937c27d731b90c000fb32b7906d81f646c
root <admin@eatlbd.com>
2023-07-28 07:11:54 +0600

    add files

---
+++ app1/src/resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>App 1</title>
+</head>
+<body>
+<div style="width: 100%; font-size: 40px; font-weight: bold; text-align: center;">
+<?php
+  echo "Hello I am App 1";
+?>
+</div>
+</body>
+</html>
---
+++ app2/src/resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>App 2</title>
+</head>
+<body>
+<div style="width: 100%; font-size: 40px; font-weight: bold; text-align: center;">
+<?php
+  echo "Hello I am App 2";
+?>
+</div>
+</body>
+</html>
